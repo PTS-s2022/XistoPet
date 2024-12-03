ae00763decd7471b7086c7b7b6df67187b0aba03
Marques-Brenno <brennomarques04@gmail.com>
2024-12-02 22:56:37 -0300

    estoque

--- public/estoque.php
+++ public/estoque.php
@@ -1,2 +1,34 @@
-<a href=""></a>
-ola
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gerenciar administradors</title> 
+    <link rel="stylesheet" href="../assets/css/stock/estoque.css">
+    <link href="https://fonts.googleapis.com/css2?family=Bangers&family=Carter+One&family=Nunito+Sans:wght@400;700&display=swap" rel="stylesheet">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+
+<body onload="Carregado()">
+  
+<?php
+    require_once('../libs/header.php');
+  ?>     
+
+  <div class="container-alto">
+      <div class="flex"> <!-- CONTEUDO DA PÁGINA -->
+        <p class="titulo1"><span>Gerenciar estoque</span></p>
+          
+        <div class="card"> <!-- GERENCIAR administrador -->
+    
+            
+        </div>
+      </div> <!-- FIM DO CONTEUDO DA PÁGINA -->
+  </div>
+
+  <?php
+  require_once('../libs/footer.html');
+  ?>
+<body>
+  <script src="gestao_administrador.js"></script>
+</html>
