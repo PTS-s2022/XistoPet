f10edbed3bc26595fa42bef84bb6c7fb52ff3116
Marques-Brenno <brennomarques04@gmail.com>
2024-12-04 10:52:07 -0300

    politica

--- public/termosCondicoes.php
+++ public/termosCondicoes.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Política de privacidade</title>
+    <title>Termos e condições</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <link rel="stylesheet" href="/header/header.css">
     <link rel="stylesheet" href="/header/header-reponsivo.css">
@@ -17,55 +17,49 @@
 ?>
 
     <div class="principal">
-        <p class="titulo">Política de privacidade </p>
+        <p class="titulo">Termos e condições</p>
         <div class="container">
-                <p class="subtitulo">SEÇÃO 1 - INFORMAÇÕES GERAIS </p>
-                <p class="texto">A presente Política de Privacidade contém informações sobre coleta, uso, armazenamento, tratamento e proteção dos dados pessoais dos usuários e visitantes do site XistoPet, com a finalidade de demonstrar absoluta transparência quanto ao assunto e esclarecer a todos interessados sobre os tipos de dados que são coletados, os motivos da coleta e a forma como os usuários podem gerenciar ou excluir as suas informações pessoais.</p>
-                <p class="texto">Esta Política de Privacidade aplica-se a todos os usuários e visitantes do site XistoPet e integra os Termos e Condições Gerais de Uso do site XistoPet, devidamente inscrita no CNPJ sob o nº 59******/0001-**, situado em Coronel Macedo – SP, Rua São Benedito 171, doravante nominada Matheus Vieira Rodrigues.</p>
-                <p class="texto">O presente documento foi elaborado em conformidade com a Lei Geral de Proteção de Dados Pessoais (Lei 13.709/18), o Marco Civil da Internet (Lei 12.965/14) (e o Regulamento da UE n. 2016/6790). Ainda, o documento poderá ser atualizado em decorrência de eventual atualização normativa, razão pela qual se convida o usuário a consultar periodicamente esta seção.</p>
-                
-        
-                <p class="subtitulo">SEÇÃO 2 - COMO RECOLHEMOS OS DADOS PESSOAIS DO USUÁRIO?</p>
-                <p class="texto">Os dados pessoais do usuário são recolhidos pela plataforma da seguinte forma:</p>
+                <p class="subtitulo">1. Aceitação dos Termos </p>
+                <p class="texto">O uso do site XIstoPet está condicionado à sua aceitação dos Termos e Condições aqui descritos. Caso não concorde com algum item, interrompa o uso do site imediatamente.</p>
+
+                <p class="subtitulo">2. Alterações nos Termos</p>
+                <p class="texto">A XIstoPet reserva-se o direito de alterar estes Termos e Condições a qualquer momento, sem aviso prévio. Recomendamos revisar este documento regularmente para se manter informado.</p>
+                        
+                <p class="subtitulo">3. Elegibilidade</p>
+                <p class="texto">Para usar os serviços do XIstoPet, você deve:</p>
                 <ul>
-                    <li>Quando o usuário cria uma conta no site.</li>
-                    <li>Quando o usuário realiza compras.</li>
-                    <li> Por intermédio de terceiro: a empresa XistoPet recebe dados de terceiros, como Google, quando um usuário faz login com o seu perfil de um desses sites. A utilização desses dados é autorizada previamente pelos usuários junto ao terceiro em questão.</li>
-                </ul>
-        
-                <p class="subtitulo">SEÇÃO 3 - QUAIS DADOS PESSOAIS RECOLHEMOS SOBRE O USUÁRIO?</p>
-                <ul>
-                    <li>Dados para a criação da conta na plataforma XistoPet: nome completo e email.</li>
-                    <li>Dados para concretizar transações: dados referentes ao pagamento e transações, tais como, cartão de crédito e outras informações sobre o cartão, além dos pagamentos efetuados, além do endereço do usuário.</li>
+                    <li>Possuir capacidade legal para celebrar contratos;</li>
+                    <li>Fornecer informações verdadeiras e atualizadas durante o cadastro.</li>
                 </ul>
                 
-                <p class="subtitulo">SEÇÃO 4 - PARA QUE FINALIDADES UTILIZAMOS OS DADOS PESSOAIS DO USUÁRIO?</p>
-                <p class="texto">Os dados pessoais do usuário e do visitante coletados e armazenados pela plataforma XistoPet tem por finalidade:</p>
+                <p class="subtitulo">4. Funcionamento da Plataforma</p>
+                <p class="texto">O XIstoPet é uma loja virtual especializada em produtos para pets, incluindo:</p>
                 <ul>
-                    <li>Bem-estar do usuário: aprimorar o produto e/ou serviço oferecido, facilitar, agilizar e cumprir os compromissos estabelecidos entre o usuário e a empresa, melhorar a experiência dos usuários e fornecer funcionalidades específicas a depender das características básicas do usuário.</li>
-                    <li>Melhorias da plataforma: compreender como o usuário utiliza os serviços da plataforma, para ajudar no desenvolvimento de negócios e técnicas.</li>
-                    <li>Comercial: os dados são usados para personalizar o conteúdo oferecido e gerar subsídio à plataforma para a melhora da qualidade no funcionamento dos serviços.</li>
-                    <li>Previsão do perfil do usuário: tratamento automatizados de dados pessoais para avaliar o uso na plataforma.</li>
-                    <li>Dados de cadastro: para permitir o acesso do usuário a determinados conteúdos da plataforma, exclusivo para usuários cadastrados.</li>
+                    <li>Rações;</li>
+                    <li>Acessórios;</li>
+                    <li>Brinquedos;</li>
+                    <li>Produtos de higiene e saúde.</li>
                 </ul>
-                <p class="texto">O tratamento de dados pessoais para finalidades não previstas nesta Política de Privacidade somente ocorrerá mediante comunicação prévia ao usuário, de modo que os direitos e obrigações aqui previstos permanecem aplicáveis.</p>
+                <p class="texto">Os produtos estão sujeitos à disponibilidade em estoque e os preços podem ser alterados sem aviso prévio.</p>
         
-                <p class="subtitulo">SEÇÃO 5 - POR QUANTO TEMPO OS DADOS PESSOAIS FICAM ARMAZENADOS?</p>
-                <p class="texto">Os dados pessoais do usuário e visitante são armazenados pela plataforma durante o período necessário para a prestação do serviço ou o cumprimento das finalidades previstas no presente documento, conforme o disposto no inciso I do artigo 15 da Lei 13.709/18.</p>
-                <p class="texto">Os dados podem ser removidos ou anonimizados a pedido do usuário, excetuando os casos em que a lei oferecer outro tratamento. Ainda, os dados pessoais dos usuários apenas podem ser conservados após o término de seu tratamento nas seguintes hipóteses previstas no artigo 16 da referida lei:</p>
-                <p>I - cumprimento de obrigação legal ou regulatória pelo controlador;</p>
-                <p>II - estudo por órgão de pesquisa, garantida, sempre que possível, a anonimização dos dados pessoais;</p>
-                <p>III - transferência a terceiro, desde que respeitados os requisitos de tratamento de dados dispostos nesta Lei;</p>
-                <p>IV - uso exclusivo do controlador, vedado seu acesso por terceiro, e desde que anonimizados os dados.</p>
+                <p class="subtitulo">5. Cadastro e Responsabilidades do Usuário </p>
+                <p class="texto">O cadastro no site XIstoPet é necessário para realizar compras e exige:</p>
+                <ul>
+                    <li>Nome completo;</li>
+                    <li>E-mail válido</li>
+                    <li>Endereço completo;</li>
+                    <li>Criação de uma senha segura.</li>
+                </ul>
+                <p class="texto">O usuário é responsável por manter suas credenciais de acesso em sigilo. Caso identifique uso não autorizado de sua conta, entre em contato imediatamente pelo e-mail xistopet@gmail.com ou pelo telefone (15) 99757-7535.</p>
+                
         
-                <p class="subtitulo">SEÇÃO 6 - SEGURANÇA DOS DADOS PESSOAIS ARMAZENADOS</p>
+                <p class="subtitulo">6. Compras e Pagamentos</p>
                 <p class="texto">Para proporcionar aos usuários da XistoPet maior segurança dos dados armazenados, foram utilizadas as técnicas de segurança:</p>
                 <ul>
-                    <li><p class="subtitulo2">SQL Injection (SQLi):</p> Exploração de falhas em consultas SQL para acessar ou manipular dados de um banco de dados. A proteção contra SQL Injection é implementada com o uso do padrão Model-View-Controller (MVC), que organiza o código em camadas separadas para maior segurança e clareza. Nesse padrão, a manipulação dos dados é realizada exclusivamente pela camada Model, que verifica o método pelo qual os dados são enviados, garantindo que apenas entradas seguras sejam processadas. Além disso, o arquivo responsável por essa manipulação é protegido contra acesso direto pelo usuário, impedindo que seja acessado ou modificado de forma não autorizada.</li>
-                    <li><p class="subtitulo2"> Cross-Site Scripting (XSS):</p> Inserção de scripts maliciosos em páginas web, afetando os usuários. Para prevenir XSS, todos os arquivos ou dados que os usuários enviam para o sistema são cuidadosamente verificados para garantir que não contenham códigos maliciosos. Isso inclui a validação e sanitização de entradas, o uso de filtros específicos para remover scripts embutidos, e a aplicação de políticas de segurança que impedem a execução de códigos perigosos no lado do cliente.</li>
-                    <li><p class="subtitulo2">Brute Force:</p> É um método de ataque que envolve tentativas sistemáticas de adivinhar senhas ou credenciais, podendo ser realizado de forma automatizada (por bots) ou manualmente por um atacante. Para proteger contra esse tipo de ataque, nosso site utiliza o reCAPTCHA do Google, que impede interações automatizadas, e implementa medidas adicionais, como bloqueio temporário de contas após várias tentativas falhas, para dificultar ataques manuais. Essas práticas garantem maior segurança às contas dos usuários. O site implementa medidas avançadas para proteger as informações dos usuários:</li>
-                    <li><p class="subtitulo2">Criptografia de Dados Sensíveis:</p> Os dados sensíveis armazenados no banco de dados SQL Server são criptografados, permitindo que sejam protegidos contra acessos não autorizados. Esses dados podem ser descriptografados somente por meio das chaves apropriadas, garantindo acesso seguro e controlado.</li>
-                    <li><p class="subtitulo2">Proteção de Senhas com Hash:</p> As senhas dos usuários são protegidas utilizando métodos de hashing, como bcrypt ou algoritmos similares. Essa técnica transforma as senhas em uma representação unidirecional, que não pode ser revertida para o valor original, mesmo em caso de acesso não autorizado ao banco de dados. Isso garante máxima segurança para as credenciais dos usuários.</li>
+                    <li><p class="subtitulo2">Preços: </p>Todos os preços incluem impostos aplicáveis e estão sujeitos a alterações sem aviso prévio.</li>
+                    <li><p class="subtitulo2">Formas de Pagamento: </p>Aceitamos cartões de crédito, PIX, boleto bancário e outras opções indicadas durante o checkout.</li>
+                    <li><p class="subtitulo2">Confirmação do Pedido: </p> O pedido será processado após a confirmação do pagamento.</li>
+                    
                 </ul>
         
                 <p class="subtitulo">SEÇÃO 7 - CONSENTIMENTO</p>
